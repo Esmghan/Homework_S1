2126ead0d8b0c02d77e4ba3863f4ad3d4b6df95f
Esmghan <11stv.ru@mail.ru>
2022-07-14 09:13:26 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число C: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if(numberB > max) max=numberB;
+if(numberC > max) max=numberC;
+
+Console.Write("max = ");
+Console.Write(max);
