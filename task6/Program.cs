f8fea4df6307d21a8a650aec3e197de829d330fd
Esmghan <11stv.ru@mail.ru>
2022-07-14 09:16:09 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int even = number % 2;
+
+if (even == 0) { 
+    Console.WriteLine("Да");
+}
+else { 
+    Console.WriteLine("Нет");
+}
